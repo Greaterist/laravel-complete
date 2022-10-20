bce6ddc7e5f98cf69c5066bf50d9cf49d74d2343
Ivan <taktaef@yandex.ru>
2022-10-20 17:14:01 +0300

    fixes

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'
+
+
+
+if (document.getElementById('mainApp')) {
+    ReactDOM.render(<App />, document.getElementById('mainApp'));
+}
