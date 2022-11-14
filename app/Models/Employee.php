cef2779341befa1761598155f3527068c5c58a0d
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2022-11-14 12:30:12 +0300

    model comments

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -7,6 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
+    //protected $table = 'Employees'; // можно выставить дефолтное название таблицы. По умолчанию берется название класса + s (Employees). А CamelCase переходит в snake_case
+
+        ///////Первичный ключ
+
+    //protected $primaryKey = 'employee_id'; // можно выставить дефолтное название первичного ключа. По умолчанию 'id'
+    //public $incrementing = false; // автоинкремент первичного ключа. По умолчанию true
+    //protected $keyType = 'string'; // тип певичного ключа. По умолчанию integer
+
+        /////// Время
+
+    //public $timestamps = false; // Штамп времени. По умолчанию true
+
     use HasFactory;
     protected $fillable = [
         'employee_name',
